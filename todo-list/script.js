3e0ddd81b2afb877daa2301c5a9c2d42e4b94df2
Michael Koek <michaelkoek@michaels-MacBook-Pro-2.local>
2024-02-07 16:39:11 +0100

    add base elements

---
+++ todo-list/script.js
@@ -0,0 +1,26 @@
+function getQuote() {
+    const listOfQuotes = [
+        {
+            quote: "Never, ever underestimate the importance of having fun.",
+            author: "I forgot",
+        },
+        {
+            quote: "I didn't have time to write a short letter, so I wrote a long one instead.",
+            author: "Mark Twain",
+        },
+        {
+            quote: "The only way to have a friend is to be one.",
+            author: "Ralph Waldo Emerson",
+        },
+        {
+            quote: "Two possibilities exist: Either we are alone in the Universe or we are not. Both are equally terrifying.",
+            author: 'Arthur C. Clarke"',
+        },
+    ];
+}
+
+function onAddItem() {}
+
+function onCheckItem() {}
+
+function onRemoveItem() {}
